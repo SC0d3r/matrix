f4783bc688307eab8c946a2da95ccb7b5dc1b006
SC0d3r <sasan.kelishani@gmail.com>
2018-05-10 04:02:06 +0430

    add scale function

---
+++ spec/scale.spec.js
@@ -0,0 +1,20 @@
+const scale = require('../src/scale');
+
+describe('scale function' , function(){
+  let m;
+  it('should throw if matrix is not valid' , function(){
+    m = [1 , [2]];
+    expect(function(){scale(m , 2)}).toThrow();
+  })
+  it('should throw if the value for scaling is not numeric' , function(){
+    m = [1,2];
+    expect(function(){scale(m , 'not a numeric value')}).toThrow();
+  })
+  it('should scale the matrix by the given number' , function(){
+    m = [1,2];
+    expect(scale(m , 3)).toEqual([3 , 6]);
+
+    m = [[1,2] , [3,4]];
+    expect(scale(m , 2)).toEqual([[2,4] ,[6,8]]);
+  })
+})
---
+++ src/scale.js
@@ -0,0 +1,42 @@
+const isValid = require('./validityCheck/isValid');
+const messages = require('./validityCheck/messages.json');
+const { flattenOneLevel, isSemi2D, isArray } = require('./utils');
+
+module.exports = function scale(m, mag = 1) {
+  if (!isValid(m))
+    throw new Error(messages.default);
+  if (!isNumeric(mag))
+    throw new Error('param passed in for scaling is not a number');
+
+  if (isSemi2D(m)) m = flattenOneLevel(m);
+
+  const isTwoDim = isArray(m[0]);
+
+  return isTwoDim ? twoDimScale(m, mag) : oneDimScale(m, mag);
+
+}
+function twoDimScale(m , mag) {
+  const res = [];
+  const cols = m[0].length;
+  const rows = m.length;
+  for (let i = 0; i < rows; i++) {
+    res[i] = [];
+    for (let j = 0; j < cols; j++) {
+      res[i][j] = m[i][j] * mag;
+    }
+  }
+  return res;
+}
+
+function oneDimScale(m , mag) {
+  const rows = m.length;
+  const res = [];
+  for (let i = 0; i < rows; i++) {
+    res[i] = m[i] * mag;
+  }
+  return res;
+}
+
+function isNumeric(maybeNumber) {
+  return !isNaN(maybeNumber) && typeof (maybeNumber) === 'number';
+}
