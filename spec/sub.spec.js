5bece06a76dfe29d13127e77c48324f5f2a2e219
SC0d3r <sasan.kelishani@gmail.com>
2018-05-10 04:01:42 +0430

    add sub function

---
+++ spec/sub.spec.js
@@ -0,0 +1,17 @@
+const sub = require('../src/sub');
+
+describe('sub function' , function(){
+  let m1,m2;
+  it('should throw if the martices passed in is invalid' , function(){
+    m1 = [1];
+    m2 = [1 , [2]];
+    expect(function(){sub(m1 , m2)}).toThrow();
+  })
+
+  it('should subtract elementwise two matrices ' , function(){
+    m1 = [1 , 2];
+    m2 = [3 , 4];
+
+    expect(sub(m1 , m2)).toEqual([-2,-2]);
+  })
+})
---
+++ src/sub.js
@@ -0,0 +1,39 @@
+const additionValidity = require('./validityCheck/additionValidity');
+const { flattenOneLevel , isSemi2D ,isArray} = require('./utils');
+const messages = require('./validityCheck/messages.json');
+
+const message = messages.default;
+
+module.exports = function sub(m1, m2) {
+  if (!additionValidity(m1, m2))
+    throw new Error(message);
+
+  if (isSemi2D(m1)) m1 = flattenOneLevel(m1);
+  if (isSemi2D(m2)) m2 = flattenOneLevel(m2);
+
+  const isTwoDim = isArray(m1[0]) || isArray(m2[0]);
+
+  return isTwoDim ? twoDimSub(m1, m2) : oneDimSub(m1, m2);
+}
+
+function twoDimSub(m1, m2) {
+  const res = [];
+  const cols = m1[0].length;
+  const rows = m1.length;
+  for (let i = 0; i < rows; i++) {
+    res[i] = [];
+    for (let j = 0; j < cols; j++) {
+      res[i][j] = m1[i][j] - m2[i][j];
+    }
+  }
+  return res;
+}
+function oneDimSub(m1, m2) {
+  const rows = m1.length;
+  const res = [];
+  for (let i = 0; i < rows; i++) {
+    res[i] = m1[i] - m2[i];
+  }
+  return res;
+}
+
