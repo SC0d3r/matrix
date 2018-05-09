224fd2bda00ac1ad2624af1d86cbf22e2cf72c07
SC0d3r <sasan.kelishani@gmail.com>
2018-05-10 03:59:51 +0430

    Update isValid : change name

--- src/validityCheck/isValid.js
+++ src/validityCheck/isValid.js
@@ -1,6 +1,6 @@
 const invalidityReasoner = require('./invalidityReasoner');
 
-module.exports  = function valid(matrix) {
+module.exports  = function isValid(matrix) {
   const invalidityReasons = invalidityReasoner(matrix);
   return invalidityReasons.length === 0;
 }
