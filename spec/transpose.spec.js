739b97811660adc84bebf63e1e816ff3f54c913b
SC0d3r <sasan.kelishani@gmail.com>
2018-05-11 01:57:21 +0430

    add transpose function

---
+++ spec/transpose.spec.js
@@ -0,0 +1,17 @@
+const transpose = require('../src/transpose');
+
+describe('transpose function', function () {
+  let m;
+  it('should throw if the matrix is not valid', function () {
+    m = [1, [33]];
+    expect(function () { transpose(m) }).toThrow();
+  });
+
+  it('should transpose the given matrix(a valid one)', function () {
+    m = [1, 2];
+    expect(transpose(m)).toEqual([[1], [2]]);
+    
+    m = [[1, 2], [3, 4]];
+    expect(transpose(m)).toEqual([[1 , 3], [2 , 4]]);
+  })
+});
---
+++ src/transpose.js
@@ -0,0 +1,35 @@
+const isValid = require('./validityCheck/isValid');
+const messages = require('./validityCheck/messages.json');
+const { isArray,columns , rows } = require('./utils');
+
+module.exports = function transpose(m) {
+  if (!isValid(m))
+    throw new Error(messages.default);
+
+  const isTwoDim = isArray(m[0]);
+
+  return isTwoDim ? twoDimTP(m) : oneDimTP(m)
+}
+
+
+function twoDimTP(m) {
+  const res = [];
+  const cols = columns(m);
+  const _rows = rows(m);
+  for (let i = 0; i < _rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      res[j] = res[j] || [];
+      res[j][i] = m[i][j];
+    }
+  }
+  return res;
+}
+
+function oneDimTP(m) {
+  const cols = columns(m);
+  const res = [];
+  for (let i = 0; i < cols; i++) {
+    res[i] = [m[i]];
+  }
+  return res;
+}
