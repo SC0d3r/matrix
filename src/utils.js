9900f236ca91ca0b69c66670b1fde873f18a24c3
SC0d3r <sasan.kelishani@gmail.com>
2018-05-11 01:25:53 +0430

    Update utils : add rows , columns functions

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,20 @@
-// turns [[1] , [2]] => into [1,2]
 const isArray = Array.isArray;
-
-exports.flattenOneLevel = function(arr){
-  const res = [];
-  for(let i = 0;i < arr.length; i++) {
-    const x = arr[i];
-    if(isArray(x)) res.push(x[0]);
-    else res.push(x);
-  }
-  return res;
-}
-
-
-exports.isSemi2D = function(arr) {
-  const hasArray = isArray(arr[0]);
-  const hasOneLengthCol = hasArray && arr[0].length === 1;
-  return hasOneLengthCol;
-}
-
 exports.isArray = Array.isArray;
+
+// every ',' in matrix array is considered a column
+exports.columns = function(ValidMatrix) {
+  const x = ValidMatrix[0];
+  if (!isArray(x)) return ValidMatrix.length;
+
+  
+  return x.length;
+}
+
+// every inner array is considered a row
+exports.rows = function(ValidMatrix) {
+  const x = ValidMatrix[0];
+  if (!isArray(x)) return 1;
+
+  
+  return ValidMatrix.length;
+}
