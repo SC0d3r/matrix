85f46be129901b807a2d95ca4c5037a968d66529
SC0d3r <sasan.kelishani@gmail.com>
2018-05-12 03:33:32 +0430

    Update strigify : fix some issues

--- spec/stringify.spec.js
+++ spec/stringify.spec.js
@@ -22,13 +22,32 @@ describe('stringify functin', function () {
     expect(strRepr).toContain('2');
     expect(strRepr).toContain('3');
     expect(strRepr).toContain('4');
-    expect(strRepr).toContain('**');
-
+    expect(strRepr).toContain('..');
+    
     m1 = [[1],[2]];
     strRepr = stringify(m1);
     
     expect(strRepr).toContain('1');
     expect(strRepr).toContain('2');
-    expect(strRepr).toContain('*');
+    expect(strRepr).toContain('.');
+    
+    m1 = [[1,2,3],[4,5,6]];
+    strRepr = stringify(m1);
+    
+    m1 = [[1,2,3,4],[4,5,6,2]];
+    strRepr = stringify(m1);
+    
+    m1 = [[1,2,3,4,5],[4,5,6,2,4]];
+    strRepr = stringify(m1);
+    
+    m1 = [[1,2,3,4,5,1],[4,5,6,2,4,7]];
+    strRepr = stringify(m1);
+    
+    m1 = [[1,2,3,4,5,1],[4,5,6,2,4,7],[4,5,6,2,4,7]];
+    strRepr = stringify(m1);
+    
+    m1 = [[1,2,3,4,5,1],[4,5,6,2,4,7],[4,5,6,2,4,7],[4,5,6,2,4,7]];
+    strRepr = stringify(m1);
+    // console.log(strRepr);
   })
 })
--- src/stringify.js
+++ src/stringify.js
@@ -11,27 +11,45 @@ module.exports = function stringify(m) {
 }
 
 function rowMatrix(m) {
+
   const cols = columns(m);
+
   let res = `[ `;
+
   for (let i = 0; i < cols; i++) {
     res += `  ${m[i]}  `;
   }
+
   res += ` ]`;
+  
   return res;
 }
 
 function stringify2D(m) {
   const cols = columns(m);
   const _rows = rows(m);
-  let res = `\n ${'_'.repeat(cols % 2 || cols)}${' '.repeat(cols)}${'_'.repeat(cols % 2 || cols)}\n`;
+
+  const howManyEdge = cols === 1 ? 1 : 2;
+
+  // this is an arithmetic progression a0 = 2 , n = cols , d = 3
+  // an = a0 + (n - 1) * d;
+  const howManySpace = Math.abs((2 + (cols - 1) * 3) - 3);
+
+  let res = `\n ${'_'.repeat(howManyEdge)}${' '.repeat(howManySpace)}${'_'.repeat(howManyEdge)}\n`;
+
   for (let i = 0; i < _rows; i++) {
+
     res += '|';
+
     for (let j = 0; j < cols; j++) {
       res += ` ${m[i][j]} `;
     }
+
     if (i !== _rows - 1) res += '|\n';
     else res += '|';
   }
-  res += `\n|${'_'.repeat(cols % 2 || cols)}${'*'.repeat(cols)}${'_'.repeat(cols % 2 || cols)}| \n`;
+
+  res += `\n|${'_'.repeat(howManyEdge)}${'.'.repeat(howManySpace)}${'_'.repeat(howManyEdge)}| \n`;
+
   return res;
 }
