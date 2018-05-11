b67ee47d8c652920d19c48ac93a00bbd01dc5d6f
SC0d3r <sasan.kelishani@gmail.com>
2018-05-11 17:11:50 +0430

    add random function

---
+++ spec/random.spec.js
@@ -0,0 +1,41 @@
+const random = require('../src/random');
+
+describe('random function',function(){
+  it('should throw if the first argument is not a number' , function(){
+    expect(function(){random('dummy')}).toThrow();
+  })
+  it('should throw if the second argument is provided and it is not a function or number' , function(){
+    expect(function(){random(1,'2')}).toThrow();
+    function rand(){};
+    expect(function(){random(1,rand)}).not.toThrow();
+  })
+  it('should throw if the third argument is provided and it is not a function' , function(){
+    expect(function(){random(1,3,4)}).toThrow();
+  })
+
+  it('should return a random x*x generated matrix with only one parameter',function(){
+    let res = random(2);
+    expect(res[0].length).toEqual(2);
+    expect(res[1].length).toEqual(2);
+
+    res = random(1);// returns [[0 <= x < 1]]
+    expect(res[0].length).toEqual(1);
+  })
+
+  it('should return a random n*m generated matrix with two parameters',function(){
+    let res = random(2,3);
+    expect(res[0].length).toEqual(3);
+    expect(res[1].length).toEqual(3);
+
+    res = random(1,2);// returns [[0 <= x1 < 1,0 <= x2 < 1]]
+    expect(res[0].length).toEqual(2);
+  });
+
+  it('should get a function and use that as a generator for random numbers',function(){
+    const rand = () => 1;
+    res = random(2,rand);
+    expect(res[0].length).toEqual(2);
+    expect(res[1].length).toEqual(2);
+    expect(res).toEqual([[1,1] ,[1,1]]);
+  })
+})
---
+++ src/random.js
@@ -0,0 +1,34 @@
+
+const isANumber = x => !isNaN(x) && typeof x === 'number';
+const isAFunction = x => typeof x === 'function';
+
+module.exports = function random(row, col, randGen) {
+  if (!isANumber(row))
+    throw new Error('first argument should be a number');
+
+  if (col !== undefined) {
+    if (isAFunction(col)) {
+      randGen = col;
+      col = row;
+    } else if (!isANumber(col))
+      throw new Error('second argument should be a number of columns');
+  }
+
+
+  if (randGen !== undefined && !isAFunction(randGen))
+    throw new Error('third argument should a function to generate random numbers');
+
+  col = col || row;
+  randGen = randGen || Math.random;
+
+  const resultMatrix = [];
+
+  for (let i = 0; i < row; i++) {
+    resultMatrix[i] = [];
+    for (let j = 0; j < col; j++) {
+      resultMatrix[i][j] = randGen(i,j);
+    }
+  }
+
+  return resultMatrix;
+}
