62803e51d0584fad072831b2760e90dc0300e047
SC0d3r <sasan.kelishani@gmail.com>
2018-05-12 03:04:22 +0430

    add index.js file

---
+++ index.js
@@ -0,0 +1,17 @@
+const add = require('./src/add');
+const sub = require('./src/sub');
+const div = require('./src/div');
+const det = require('./src/det');
+const identity = require('./src/identity');
+const mul = require('./src/mul');
+const random = require('./src/random');
+const scale = require('./src/scale');
+const transpose = require('./src/transpose');
+const { row, rows, column, columns } = require('./src/utils');
+
+module.exports = {
+  add, sub, div,
+  det, identity, mul,
+  random, scale, transpose,
+  row, rows, column, columns
+}
