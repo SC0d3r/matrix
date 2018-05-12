0cb7362fd189ec1c00098deee18778a8c2e5e4f0
SC0d3r <sasan.kelishani@gmail.com>
2018-05-13 04:01:26 +0430

    Update mul : add precision as thrid argument

--- src/mul.js
+++ src/mul.js
@@ -2,22 +2,26 @@ const isValid = require('./validityCheck/isValid');
 const messages = require('./validityCheck/messages.json');
 const { isArray, columns, rows } = require('./utils');
 
-module.exports = function mul(m1, m2) {
+const fixed = x => num => (+num.toFixed(x));
+
+module.exports = function mul(m1, m2, precision = 5) {
   if (!isValid(m1) || !isValid(m2))
     throw new Error(messages.default);
 
   if (!validDimforMul(m1, m2))
     throw new Error('Matrices dont have valid dim for multiplication');
 
+
   const isTwoDim = columns(m2) !== 1;
 
-  return isTwoDim ? twoDimMul(m1, m2) : oneDimMul(m1, m2);
+  return isTwoDim ? twoDimMul(m1, m2, precision) : oneDimMul(m1, m2, precision);
 }
 
-function twoDimMul(m1, m2) {
+function twoDimMul(m1, m2 , precision) {
   const res = [];
   const resultM_rows = rows(m1);
   const resultM_cols = columns(m2);
+  const fixedPrecision = fixed(precision);
 
   const m1Cols = columns(m1);
   for (let i = 0; i < resultM_rows; i++) {
@@ -27,20 +31,22 @@ function twoDimMul(m1, m2) {
       for (let k = 0; k < m1Cols; k++) {
         temp += m1[i][k] * m2[k][j];
       }
-      res[i][j] = temp;
+      res[i][j] = fixedPrecision(temp);
     }
   }
   return res;
 }
-function oneDimMul(m1, m2) {
+function oneDimMul(m1, m2 , precision) {
   let res = 0;
+
+  const fixedPrecision = fixed(precision);
 
   const m1Cols = columns(m1);
   for (let i = 0; i < m1Cols; i++) {
     res += m1[i] * m2[i][0];
   }
 
-  return [res];
+  return [fixedPrecision(res)];
 }
 
 function validDimforMul(m1, m2) {
