9c1ed7c7dfe12497cdda3788aa2f6c96f2dea2db
SC0d3r <sasan.kelishani@gmail.com>
2018-05-17 02:09:10 +0430

    sub : add more tests

--- spec/sub.spec.js
+++ spec/sub.spec.js
@@ -1,17 +1,22 @@
 const sub = require('../sub');
 
-describe('sub function' , function(){
-  let m1,m2;
-  it('should throw if the martices passed in is invalid' , function(){
+describe('sub function', function () {
+  let m1, m2;
+  it('should throw if the martices passed in is invalid', function () {
     m1 = [1];
-    m2 = [1 , [2]];
-    expect(function(){sub(m1 , m2)}).toThrow();
+    m2 = [1, [2]];
+    expect(function () { sub(m1, m2) }).toThrow();
   })
 
-  it('should subtract elementwise two matrices ' , function(){
-    m1 = [1 , 2];
-    m2 = [3 , 4];
+  it('should subtract elementwise two matrices ', function () {
+    m1 = [1, 2];
+    m2 = [3, 4];
 
-    expect(sub(m1 , m2)).toEqual([-2,-2]);
+    expect(sub(m1, m2)).toEqual([-2, -2]);
+
+    m1 = [[1, 2], [-1, 5]];
+    m2 = [[3, 4], [3, -1]];
+
+    expect(sub(m1, m2)).toEqual([[-2, -2], [-4, 6]]);
   })
 })
